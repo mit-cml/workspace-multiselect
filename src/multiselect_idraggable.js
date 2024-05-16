aaa8f82e4a4044126a583206f1e730630ab42ec2
Chang Min Bark <changminbark@gmail.com>
2024-05-16 14:43:09 -0400

    created boilerplate/skeleton for multiselect-draggable

--- src/multiselect_idraggable.js
+++ src/multiselect_idraggable.js
@@ -7,25 +7,29 @@
 /**
  * @fileoverview Multiple selection draggable class.
  */
-
 import * as Blockly from 'blockly/core';
+// This import is only if I decide to extend the MultiDraggable class
+import {MultiDraggable} from "./multi_draggable";
+import {Coordinate} from "blockly/core/utils/coordinate";
 
 /**
  * A draggable object that adds the functionality for multiple blocks to
  * be moved while someone is dragging it. It implements the IDraggable interface in Blockly v11.
  */
-export class MultiselectDraggable implements Blockly.IDraggable {
-    private subDraggables: Blockly.IDraggable[];
+export class MultiselectDraggable {
 
     // TODO: Need to determine what is needed in constructor
-    constructor(workspace) {
+    constructor(workspace, subDraggables) {
         this.workspace_ = workspace;
-
+        this.subDraggables = subDraggables;
+        this.view
+        this.svgRoot
+        this.loc
     }
 
     // TODO: Need to determine how to implement this after talking to mentor
-    addSubDraggable(subBlock: Blockly.IDraggable) {
-        this.subDraggables.push(subBlock)
+    addSubDraggable(subBlock) {
+
     }
 
     // TODO: Need to determine if blocks will always be movable
@@ -33,31 +37,36 @@ export class MultiselectDraggable implements Blockly.IDraggable {
         return true;
     }
 
-    // TODO: Need to determine whether these are needed
+    /**
+     * Returns the root SVG element of the multiselect-draggable.
+     * @returns {SVGElement} The root SVG element of this rendered multiselect-draggable element.
+     */
+    getSvgRoot() {
 
-    // getRelativeToSurfaceXY(): Coordinate {
-    //     return this.loc;
-    // }
+    }
 
-    // dispose() {
-    //
-    // }
+    // TODO: Complete these methods
 
-    // revertDrag() {
-    // }
+    /**
+     * Returns the current location of the multiselect-draggable in workspace coordinates.
+     * @returns {Coordinate} The coordinate of the current location on workspace (top left corner of multiselect-draggable)
+     */
+    getRelativeToSurfaceXY() {
+        return this.loc;
+    }
 
 
     // TODO: Need to implement startDrag
-    startDrag(e: PointerEvent) {
+    startDrag(e) {
       for (const draggable of this.subDraggables) {
         draggable.startDrag(e);
       }
     }
 
     // TODO: Need to implement drag
-    drag(newLoc: Coordinate, target: IDragTarget, e?: PointerEvent) {
+    drag(newLoc, e, target) {
       for (const draggable of this.subDraggables) {
-        draggable.drag(Blockly.Coordinate.sum(newLoc, draggable.dragOffset), target, e);
+        draggable.drag(Coordinate.sum(newLoc, draggable.dragOffset), target, e);
       }
     }
 
@@ -67,6 +76,37 @@ export class MultiselectDraggable implements Blockly.IDraggable {
         draggable.endDrag(e);
       }
     }
+
+    // TODO: Need to implement revertDrag
+    revertDrag() {
+
+    }
+
+    // BELOW ARE OTHER INTERFACES THAT MAY NEED TO BE IMPLEMENTED
+    // ==========================================================
+    // ==========================================================
+    // ==========================================================
+    // ==========================================================
+    // TODO: Determine if need to implement ISelectable interface
+
+
+
+    // TODO: Determine if need to implement IDeletable interface
+
+    isDeletable() {
+        return true;
+    }
+
+    dispose() {
+
+    }
+
+    setDeleteStyle() {
+        // Visually indicate that the draggable is about to be deleted.
+    }
+
+    // TODO: Determine if need to implement ICopyable interface
+
 
 
   }
