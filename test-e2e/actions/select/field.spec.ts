6d67701efe8916a8b5bad6981600e22e16fba94b
Michael Gallagher <mjgallag@gmail.com>
2026-03-29 11:53:32 -0700

    Add single-select field edit tests

---
+++ test-e2e/actions/select/field.spec.ts
@@ -0,0 +1,46 @@
+import { expect } from "@playwright/test";
+import {
+	getBlock,
+	getBlockField,
+	getBlockFieldValue,
+	getSelectedBlockIds,
+	loadBlocks,
+	test,
+} from "../../test";
+
+test("edit boolean field", async ({ page, act }) => {
+	await act(
+		loadBlocks(page, [
+			{ type: "logic_boolean", id: "block1" },
+			{ type: "logic_boolean", id: "block2" },
+		]),
+	);
+	await act(page.mouse.click(...(await getBlock(page, "block1"))));
+
+	await act(page.mouse.click(...(await getBlockField(page, "block1", "BOOL"))));
+	expect(await getSelectedBlockIds(page)).toEqual(["block1"]);
+	await act(page.getByRole("option", { name: "false", exact: true }).click());
+
+	expect(await getBlockFieldValue(page, "block1", "BOOL")).toBe("FALSE");
+	expect(await getBlockFieldValue(page, "block2", "BOOL")).toBe("TRUE");
+	expect(await getSelectedBlockIds(page)).toEqual(["block1"]);
+});
+
+test("edit number field", async ({ page, act }) => {
+	await act(
+		loadBlocks(page, [
+			{ type: "math_number", id: "block1" },
+			{ type: "math_number", id: "block2" },
+		]),
+	);
+	await act(page.mouse.click(...(await getBlock(page, "block1"))));
+
+	await act(page.mouse.click(...(await getBlockField(page, "block1", "NUM"))));
+	expect(await getSelectedBlockIds(page)).toEqual(["block1"]);
+	await act(page.keyboard.type("42"));
+	await act(page.keyboard.press("Enter"));
+
+	expect(await getBlockFieldValue(page, "block1", "NUM")).toBe(42);
+	expect(await getBlockFieldValue(page, "block2", "NUM")).toBe(0);
+	expect(await getSelectedBlockIds(page)).toEqual(["block1"]);
+});
